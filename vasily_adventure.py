0a46aca66e0fd89b01455c0a5be0f7ad3f06865b
Radomir <radomirbozhevolnov@gmail.com>
2025-12-07 16:34:13 +0400

    [feat] поправили высоту экрана

--- vasily_adventure.py
+++ vasily_adventure.py
@@ -1,15 +1,15 @@
-import pygame
-import sys
-import os
 import math
 import random
+import sys
+
+import pygame
 
 # Инициализация Pygame
 pygame.init()
 
 # Настройки экрана
 SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 700
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Приключения Василия - Финальная версия (F11 - полноэкранный режим)")
 
